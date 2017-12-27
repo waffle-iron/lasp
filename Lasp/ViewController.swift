7a1c8d97bc9f6ecc9ed355a969a13f86f0d8b391
Francis Pineda <frpine@outlook.com>
2017-12-27 22:26:19 +0800

    Initial Commit

---
+++ Lasp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Lasp
+//
+//  Created by Francis Pineda on 27/12/2017.
+//  Copyright © 2017 Leaky Faucet. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
