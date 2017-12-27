824170af3e7a578890e0ddeff80e299a707183ea
Francis Pineda <frpine@outlook.com>
2017-12-28 00:15:31 +0800

    Delete ViewController and create HomePageViewController

---
+++ Lasp/HomePageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageViewController.swift
+//  Lasp
+//
+//  Created by Francis Pineda on 28/12/2017.
+//  Copyright © 2017 Leaky Faucet. All rights reserved.
+//
+
+import Cocoa
+
+class HomePageViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
