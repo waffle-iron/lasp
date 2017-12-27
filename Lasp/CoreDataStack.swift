7bfdaadb7d0c02f95117341c2d4338919a99201f
Francis Pineda <frpine@outlook.com>
2017-12-27 23:26:17 +0800

    Create Core Data configuration file to initialise a NSManagedObjectContext object

---
+++ Lasp/CoreDataStack.swift
@@ -0,0 +1,33 @@
+//
+//  CoreDataStack.swift
+//  Lasp
+//
+//  Created by Francis Pineda on 27/12/2017.
+//  Copyright © 2017 Leaky Faucet. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+func createMainContext() -> NSManagedObjectContext {
+    
+    let modelURL = Bundle.main.url(forResource: "Lasp", withExtension: "momd")
+    guard let model = NSManagedObjectModel(contentsOf: modelURL!) else { fatalError("model not found!") }
+    
+    let psc = NSPersistentStoreCoordinator(managedObjectModel: model)
+    let storeURL = URL.documentsURL.appendingPathComponent("Lasp.sqlite")
+    try! psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+    
+    let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    context.persistentStoreCoordinator = psc
+    
+    return context
+}
+
+extension URL {
+    static var documentsURL: URL {
+        return try! FileManager
+            .default
+            .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    }
+}
